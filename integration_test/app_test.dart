511367f3df4899f2388791e834b479c31f711779
DK-codeur <47182035+DK-codeur@users.noreply.github.com>
2022-04-11 02:17:29 +0000

    part3 i_test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testing_tutorial/article.dart';
+import 'package:flutter_testing_tutorial/article_page.dart';
 import 'package:flutter_testing_tutorial/news_change_notifier.dart';
 import 'package:flutter_testing_tutorial/news_page.dart';
 import 'package:flutter_testing_tutorial/news_service.dart';
@@ -12,8 +11,7 @@ import 'package:provider/provider.dart';
 
 class MockNewsService extends Mock implements NewsService {}
 
-//t_s
-//widget test
+//itest: test on device(real/virtual): action clic...
 void main() {
   late MockNewsService mockNewsService;
 
@@ -33,15 +31,6 @@ void main() {
     );
   }
 
-  void arrangeNewsServiceReturns3ArticlesAfter2secondWait() {
-    when(() => mockNewsService.getArticles()).thenAnswer(
-      (_) async {
-        await Future.delayed(const Duration(seconds: 2));
-        return articlesFromService;
-      }
-    );
-  }
-
   Widget createWidgetUnderTest() { //pour que l'app soit bien initialiser avec le provider (consumer)
     return MaterialApp(
       title: 'News App',
@@ -53,42 +42,23 @@ void main() {
   }
 
   testWidgets(
-    "title is displayed",
-    (WidgetTester tester) async {
-      arrangeNewsServiceReturns3Articles();
-      await tester.pumpWidget(createWidgetUnderTest()); //pump
-      expect(find.text("News"), findsOneWidget);
-    },
-  );
-
-  testWidgets(
-    "loading indicator is displayed while waiting for articles",
-    (WidgetTester tester) async {
-      arrangeNewsServiceReturns3ArticlesAfter2secondWait();
-
-      await tester.pumpWidget(createWidgetUnderTest());
-      await tester.pump(const Duration(milliseconds: 500));
-
-      // expect(find.byType(CircularProgressIndicator), findsOneWidget);
-      expect(find.byKey(Key("progress-indicator")), findsOneWidget);
-
-      tester.pumpAndSettle(); //attend qu'il ny ait plus d'animation: le loader dans notre cas
-    },
-  );
-
-
-  testWidgets(
-    "articles are displayed",
+    "tapping on the first article excerpt opens the article page where the full article content is displayed ",
     (WidgetTester tester) async {
       arrangeNewsServiceReturns3Articles();
 
       await tester.pumpWidget(createWidgetUnderTest());
       await tester.pump();
 
-      for(final article in articlesFromService) {
-        expect(find.text(article.title), findsOneWidget);
-        expect(find.text(article.content), findsOneWidget);
-      }
+      await tester.tap(find.text("test 1 content"));
+
+      await tester.pumpAndSettle(); //attend que l'animation de navigation finisse
+
+      //une fois la navigation terminer 
+      expect(find.byType(NewsPage), findsNothing); //on s'assure que nous ne somme plus sur NewsPage
+      expect(find.byType(ArticlePage), findsOneWidget); //mais plutot sur la page ArticlePage
+
+      expect(find.text("test 1"), findsOneWidget);
+      expect(find.text("test 1 content"), findsOneWidget);
     },
   );
 }
